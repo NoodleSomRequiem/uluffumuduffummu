efcd43d15ff965073f016ffef3c28a605900a3d3
Sem <89838880+NoodleSomRequiem@users.noreply.github.com>
2022-01-20 12:09:07 +0100

    Delete database.php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host       = "localhost";
-$database   = "aritec";
-$user       = "root";
-$password   = "";
-
-$db = mysqli_connect($host, $user, $password, $database)
-or die("Error: " . mysqli_connect_error());;
